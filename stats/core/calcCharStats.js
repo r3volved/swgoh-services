203bda984e579828c86819931ddc86406c1b5b0e
r3volved <32720839+r3volved@users.noreply.github.com>
2019-04-25 14:24:23 -0400

    Add files via upload

--- stats/core/calcCharStats.js
+++ stats/core/calcCharStats.js
@@ -9,27 +9,27 @@ module.exports = async ( units ) => {
     var STATS = []
 
     units.filter(u => u.combatType === 1).forEach( toon => {
-    
-        if( toon.combatType === 2 ) return 
+
+        if( toon.combatType === 2 ) return
 
         var finalStats = {}
 
         var unit = unitsList.find(u => u.id.includes(toon.defId) && u.rarity === toon.rarity)
         var sProgression = statProgressionList.find(s => s.id === unit.statProgressionId)
-        
+
         for( var i = 1; i < stats.length; ++i ) {
 
             var div = 100000000
 
             //BASE STAT
             var BS = base[i] + ((unit.baseStat.statList.find(s => s.unitStat == i) || {}).unscaledDecimalValue / div || 0)
-            
+
             //STAT CONTRIBUTION
             var SC = (sProgression.stat.statList.find(sd => sd.unitStat == i) || {}).unscaledDecimalValue / div || 0
 
             //GEAR CONTRIBUTION
             var GS = (unit.unitTierList.find(t => t.tier === toon.gear).baseStat.statList.find(s => s.unitStat == i) || {}).unscaledDecimalValue / div || 0
-            
+
             //EQUIPPED GEAR
             var EQ = toon.equipped.reduce((sum,eq) => {
                 var pc = equipmentList.find(eqp => eqp.id === eq.equipmentId)
@@ -40,29 +40,29 @@ module.exports = async ( units ) => {
             //STAT MODIFIERS
             if( i >= 2 && i <= 4 ) { BS = Math.floor(( SC * toon.level ) + GS) }
             else BS = pct[i] ? (GS || BS) : Math.floor(GS || BS)
-            
+
             finalStats[stats[i]] = { base:BS, gear:EQ, mods:0, final:0, pct:pct[i] }
-            
+
         }
 
         sProgression = null
-        
+
         for( var i = 1; i < stats.length; ++i ) {
-            
+
             var div = pct[i] ? 1000000 : 100000000
 
-            if( i === 1 ) finalStats[stats[i]].base += ( 18.0 * finalStats["Strength"].base )
-            if( i === 6 ) finalStats[stats[i]].base += ( 1.4 * finalStats[stats[unit.primaryUnitStat]].base )
-            if( i === 7 ) finalStats[stats[i]].base += ( 2.4 * finalStats["Intelligence"].base )
-            
-            if( i === 8 ) finalStats[stats[i]].base += ( 0.14 * finalStats["Strength"].base )
-            if( i === 8 ) finalStats[stats[i]].base += ( 0.07 * finalStats["Agility"].base )
-            if( i === 9 ) finalStats[stats[i]].base += ( 0.1 * finalStats["Intelligence"].base )
-            
-            if( i === 8 || i == 9 ) finalStats[stats[i]].base = Math.floor(finalStats[stats[i]].base)
-        
+            if( i === 1 ) finalStats[stats[i]].base += ( 18.0 * (finalStats["Strength"].base + finalStats["Strength"].gear)  )
+            if( i === 6 ) finalStats[stats[i]].base += ( 1.4 * (finalStats[stats[unit.primaryUnitStat]].base + finalStats[stats[unit.primaryUnitStat]].gear))
+            if( i === 7 ) finalStats[stats[i]].base += ( 2.4 * (finalStats["Intelligence"].base + finalStats["Intelligence"].gear) )
 
-            if( i === 14 ) finalStats[stats[i]].base += ( 0.4 * finalStats["Agility"].base )
+            if( i === 8 ) finalStats[stats[i]].base += ( 0.14 * (finalStats["Strength"].base + finalStats["Strength"].gear))
+            if( i === 8 ) finalStats[stats[i]].base += ( 0.07 * (finalStats["Agility"].base + finalStats["Agility"].gear))
+            if( i === 9 ) finalStats[stats[i]].base += ( 0.1 * (finalStats["Intelligence"].base + finalStats["Intelligence"].gear))
+
+            if( i === 8 || i == 9 ) finalStats[stats[i]].base = Math.floor(finalStats[stats[i]].base)
+
+
+            if( i === 14 ) finalStats[stats[i]].base += ( 0.4 * (finalStats["Agility"].base + finalStats["Agility"].gear))
             if( i === 18 ) finalStats[stats[i]].base += 0.15
 
             //mods : MODS
@@ -75,15 +75,15 @@ module.exports = async ( units ) => {
                     return ssum + ( ss.unitStat === i ? ss.value : 0 )
                 },0)
             },0) || 0
-            
-            
+
+
             // incorporate set modsets into stats totals
             var modset = modSets.find(ms => Object.keys(setCount).includes(ms.id) && ms.completeBonus.stat.unitStat === i)
             if( modset && setCount[modset.id].length >= modset.setCount ) {
                 var mult = Math.floor(modset.setCount / setCount[modset.id].length)
                 var maxd = Math.floor(modset.setCount / setCount[modset.id].filter(s => s === 15).length)
                 mult -= maxd
-                
+
                 var sdiv = pct[modset.completeBonus.stat.unitStat] ? 1000000 : 100000000
                 mods += maxd && maxd > 0 ? modset.completeBonus.stat.unscaledDecimalValue / sdiv * 2 * maxd : 0
                 mods += mult && mult > 0 ? modset.completeBonus.stat.unscaledDecimalValue / sdiv * mult : 0
@@ -92,19 +92,19 @@ module.exports = async ( units ) => {
             var statName = stats[i]
             if (statName.slice(-1) == "%") { // percent stat
                 statName = statName.replace(" %","").trim()
-                
+
                 //mods = mods / 1000000
                 //if( mods ) console.log(stats[i], mods)
                 mods = mods / 100
-                
+
                 switch (statName) {
                     case "Offense":
                         finalStats["Physical Damage"].mods += finalStats["Physical Damage"].base * mods
                         finalStats["Special Damage"].mods += finalStats["Special Damage"].base * mods
                         break;
                     case "Defense":
-                        finalStats["Armor"].mods += finalStats["Armor"].base * mods 
-                        finalStats["Resistance"].mods += finalStats["Resistance"].base * mods 
+                        finalStats["Armor"].mods += finalStats["Armor"].base * mods
+                        finalStats["Resistance"].mods += finalStats["Resistance"].base * mods
                         break;
                     case "Potency":
                     case "Tenacity":
@@ -116,10 +116,10 @@ module.exports = async ( units ) => {
                         finalStats[statName].mods += finalStats[statName].base * mods
                 }
             } else { // flat stat
-                
+
                 //mods = mods / 100000000
                 //if( mods ) console.log(stats[i], mods)
-                
+
                 switch (statName) {
                     case "Offense":
                         finalStats["Physical Damage"].mods += mods
@@ -144,54 +144,55 @@ module.exports = async ( units ) => {
             }
 
         }
-        
+
         for( var i = 1; i < stats.length; ++i ) {
-        
+
             if( i === 21 || i == 22 ) continue
-        
+
             //FINAL STAT
-            finalStats[stats[i]].base = finalStats[stats[i]].base && finalStats[stats[i]].base.toString().includes(".") ? Number(finalStats[stats[i]].base.toFixed(4)) : finalStats[stats[i]].base
-            finalStats[stats[i]].mods = finalStats[stats[i]].mods && finalStats[stats[i]].mods.toString().includes(".") ? Number(finalStats[stats[i]].mods.toFixed(4)) : finalStats[stats[i]].mods
-            
+            finalStats[stats[i]].base = finalStats[stats[i]].base && finalStats[stats[i]].base.toString().includes(".") ? Number(finalStats[stats[i]].base.toFixed(4)) : Math.floor(finalStats[stats[i]].base)
+            finalStats[stats[i]].gear = finalStats[stats[i]].gear && finalStats[stats[i]].gear.toString().includes(".") ? Number(finalStats[stats[i]].gear.toFixed(4)) : Math.floor(finalStats[stats[i]].gear)
+            finalStats[stats[i]].mods = finalStats[stats[i]].mods && finalStats[stats[i]].mods.toString().includes(".") ? Number(finalStats[stats[i]].mods.toFixed(4)) : Math.floor(finalStats[stats[i]].mods)
+
             //Accumulate gear and mods
             if( stats[i].slice(-6) == "Rating" && finalStats[stats[i].slice(0,-6) + "Chance"] ) {
-                
+
                 finalStats[stats[i].slice(0,-6) + "Chance"].base = convertFlatCritToPercent(finalStats[stats[i]].base)
                 finalStats[stats[i].slice(0,-6) + "Chance"].final += convertFlatCritToPercent(finalStats[stats[i]].base + finalStats[stats[i]].gear)
                 finalStats[stats[i].slice(0,-6) + "Chance"].final += finalStats[stats[i].slice(0,-6) + "Chance"].mods
                 delete finalStats[stats[i]]
-                
+
             } else if ( i === 8 || i === 9 ) {
-                
+
                 finalStats[stats[i]].final += finalStats[stats[i]].base + finalStats[stats[i]].gear
-                
+
                 var nonMod = convertFlatDefToPercent( finalStats[stats[i]].final, toon.level, 1)
                 finalStats[stats[i]].mods = Math.floor(finalStats[stats[i]].mods)
                 finalStats[stats[i]].final = convertFlatDefToPercent( finalStats[stats[i]].final + finalStats[stats[i]].mods, toon.level)
                 finalStats[stats[i]].mods = finalStats[stats[i]].final - nonMod
-                
+
             } else {
                 finalStats[stats[i]].final += finalStats[stats[i]].base + finalStats[stats[i]].gear
                 finalStats[stats[i]].final += finalStats[stats[i]].mods
             }
-            
-            
+
+
             if( !finalStats[stats[i]] || stats[i].includes("_") || finalStats[stats[i]].final <= 0 ) delete finalStats[stats[i]]
-            else finalStats[stats[i]].final = pct[i] ? Number(finalStats[stats[i]].final.toFixed(4)) : Math.floor(finalStats[stats[i]].final)
-            
+            else finalStats[stats[i]].final = finalStats[stats[i]].pct ? Number(finalStats[stats[i]].final.toFixed(4)) : Math.floor(finalStats[stats[i]].final)
+
         }
-        
+
         //console.log( { unit:toon.defId, stats:finalStats } )
-        
+
         STATS.push({ unit:toon.defId, stats:Object.assign({},finalStats) })
-    
+
         finalStats = null
         unit    = null
 
     })
-    
+
     return STATS
-    
+
 }
 
 function convertFlatDefToPercent(value, level = 85, scale = 1) {
@@ -201,4 +202,3 @@ function convertFlatDefToPercent(value, level = 85, scale = 1) {
 function convertFlatCritToPercent(value, scale = 1) {
   return ((value / scale)/2400 + 0.1) * scale;//.toFixed(4);
 }
-
--- stats/core/calcShipStats.js
+++ stats/core/calcShipStats.js
@@ -9,9 +9,9 @@ module.exports = async ( ships ) => {
     var STATS = []
 
     ships.forEach( ship => {
-        
-        if( ship.combatType === 1 ) return 
-        
+
+        if( ship.combatType === 1 ) return
+
         var finalStats = {}
 
         //CREW RATING
@@ -21,28 +21,18 @@ module.exports = async ( ships ) => {
 
             var uCR = 0
             uCR += tables.levelTable[u.level]
-            uCR += tables.rarityTable[u.rarity]    
+            uCR += tables.rarityTable[u.rarity]
             for( var g = 1; g < u.gear; ++g ) {
             	uCR += tables.gearTable[g] * 6
             }
             uCR += tables.gearTable[u.gear] * u.equipped.length
             u.skills.forEach( s => {
-                var iskill = index_skills.find(isk => isk.id === s.id)
-                var otag = iskill.tiers[0].powerOverrideTag || ""
-                if( s.id.includes('contract') || otag.includes('contract') ) {
-                	uCR += tables.contractTable[(s.tier || 0)]
-                } else if( s.id.includes('hardware') || otag.includes('hardware') ) {
-                	uCR += tables.reinforcementTable[(s.tier || 0)]
-                } else {
-                	uCR += s.tier === 8 && s.isZeta 
-                	    ? tables.abilityTable[s.tier] 
-                	    : tables.abilityTable[(s.tier || 0)]
-                }
+              uCR += tables.abilityTable[s.tier]
             })
             u.mods.forEach(m => {
                 //var key = m.pips+":"+m.level+":"+m.tier+":"+m.set
                 //uCR +=parseInt(tables.modTable.find(mt => mt.key === key).value)
-                uCR += parseInt( tables.crewModTable[m.level][m.pips] )                
+                uCR += parseInt( tables.crewModTable[m.level][m.pips] )
                 //console.log( parseInt(tables.modTable.find(mt => mt.key === key).value), parseInt( tables.crewModTable[m.level][m.pips] ) )
             })
 
@@ -67,86 +57,84 @@ module.exports = async ( ships ) => {
 
             //BASE STAT
             var BS = unit.baseStat.statList.find(s => s.unitStat == i)
-                BS = BS ? BS.unscaledDecimalValue / div : 0    
+                BS = BS ? BS.unscaledDecimalValue / div : 0
                 BS += base[i]
-            
+
             //STAT CONTRIBUTION
             var sls = sProgression.stat.statList.find(sd => sd.unitStat == i)
             var SC = sls ? sls.unscaledDecimalValue / div : 0
-             
+
             var cls = cProgression.stat.statList.find(sd => sd.unitStat == i)
                 SC += cls ? cls.unscaledDecimalValue / div : 0
-             
+
             //STAT MODIFIERS
-            if( i >= 2 && i <= 4 ) { 
+            if( i >= 2 && i <= 4 ) {
                 //console.log( unit.baseId, BS, SC * ship.level, sProgression.stat.statList.map(sl => sl.unitStat) )
-                BS += Math.floor( SC * ship.level ); SC = 0 
+                BS += Math.floor( SC * ship.level ); SC = 0
             }
             else BS = pct[i] ? BS : Math.floor(BS)
-            
+
             finalStats[stats[i]] = { base:Number(BS), contribution:Number(SC), bonus:0, final:0, pct:pct[i] }
         }
 
 
         for( var i = 1; i < stats.length; ++i ) {
-            
+
             if( stats[i].includes("_") ) continue
 
             if( i === 1 ) finalStats[stats[i]].base += ( 18.0 * finalStats["Strength"].base )
             if( i === 6 ) finalStats[stats[i]].base += ( 1.4 * finalStats[stats[unit.primaryUnitStat]].base )
             if( i === 7 ) finalStats[stats[i]].base += ( 2.4 * finalStats["Intelligence"].base )
-            
+
             if( i === 8 ) finalStats[stats[i]].base += ( 0.14 * finalStats["Strength"].base )
             if( i === 8 ) finalStats[stats[i]].base += ( 0.07 * finalStats["Agility"].base )
             if( i === 9 ) finalStats[stats[i]].base += ( 0.1 * finalStats["Intelligence"].base )
-            
+
             if( i === 8 || i == 9 ) finalStats[stats[i]].base = Math.floor(finalStats[stats[i]].base)
-        
+
             if( i === 14 ) finalStats[stats[i]].base += ( 0.4 * finalStats["Agility"].base )
 
         }
-                
+
         for( var i = 1; i < stats.length; ++i ) {
 
             if( stats[i].includes("_") ) continue
             if( i === 21 || i == 22 ) continue
-            
+
             //FINAL STAT
             finalStats[stats[i]].base = finalStats[stats[i]].base && finalStats[stats[i]].base.toString().includes(".") ? Number(finalStats[stats[i]].base.toFixed(4)) : finalStats[stats[i]].base
-            
+
             //SHIP BONUS
-            finalStats[stats[i]].bonus = pct[i] 
+            finalStats[stats[i]].bonus = pct[i]
                 ? Number((Number(CR) * Number(SM) * finalStats[stats[i]].contribution).toFixed(4))
                 : Math.floor(Number(CR) * Number(SM) * finalStats[stats[i]].contribution)
 
             if( stats[i].slice(-6) == "Rating" && finalStats[stats[i].slice(0,-6) + "Chance"] ) {
-                
+
                 finalStats[stats[i].slice(0,-6) + "Chance"].base = convertFlatCritToPercent( finalStats[stats[i]].base )
                 finalStats[stats[i].slice(0,-6) + "Chance"].final = convertFlatCritToPercent( finalStats[stats[i]].base + finalStats[stats[i]].bonus )
                 finalStats[stats[i].slice(0,-6) + "Chance"].bonus = finalStats[stats[i].slice(0,-6) + "Chance"].final - finalStats[stats[i].slice(0,-6) + "Chance"].base
 
                 delete finalStats[stats[i].slice(0,-6) + "Chance"].contribution
                 delete finalStats[stats[i]]
-                
-            } else if( i === 8 || i == 9 ) {
-                
-                finalStats[stats[i]].bonus = (Number(CR) * Number(SM) * finalStats[stats[i]].contribution) / 1000
-                finalStats[stats[i]].base = convertFlatDefToPercent( finalStats[stats[i]].base, ship.level, 1)     
-                           
-                finalStats[stats[i]].final = finalStats[stats[i]].base + finalStats[stats[i]].bonus
 
-                delete finalStats[stats[i]].contribution
+            } else if( i === 8 || i == 9 ) {
+
+              finalStats[stats[i]].final = convertFlatDefToPercent(finalStats[stats[i]].base + Math.floor(Number(CR) * Number(SM) * finalStats[stats[i]].contribution), ship.level, 1)
+              finalStats[stats[i]].base = convertFlatDefToPercent( finalStats[stats[i]].base, ship.level, 1)
+              finalStats[stats[i]].bonus = finalStats[stats[i]].final - finalStats[stats[i]].base
+              delete finalStats[stats[i]].contribution
 
             } else {
 
                 //FINAL SHIP STAT
-                finalStats[stats[i]].final = pct[i] 
+                finalStats[stats[i]].final = pct[i]
                     ? Number(finalStats[stats[i]].base) + Number(finalStats[stats[i]].bonus)
                     : Math.floor(Number(finalStats[stats[i]].base) + Number(finalStats[stats[i]].bonus))
 
                 delete finalStats[stats[i]].contribution
             }
-                        
+
             if( !finalStats[stats[i]] || stats[i].includes("_") || finalStats[stats[i]].final <= 0 ) delete finalStats[stats[i]]
             else finalStats[stats[i]].final = pct[i] ? Number(finalStats[stats[i]].final.toFixed(4)) : Math.floor(finalStats[stats[i]].final)
 
@@ -155,9 +143,10 @@ module.exports = async ( ships ) => {
         //console.log( {unit:ship.defId, stats:finalStats} )
 
         STATS.push({unit:ship.defId, stats:Object.assign({},finalStats) })
-    
+
         SM = null
         finalStats = null
+        cProgression = null
         sProgression = null
         unit    = null
     })
@@ -173,4 +162,3 @@ function convertFlatDefToPercent(value, level = 85, scale = 1) {
 function convertFlatCritToPercent(value, scale = 1) {
   return ((value / scale)/2400 + 0.1) * scale;//.toFixed(4);
 }
-
